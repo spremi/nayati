fc9999f57f66e42135dea1c4833d22380ffe382a
Sanjeev Premi <spremi@ymail.com>
2016-01-01 14:44:58 +0530

    app: Add activity 'ItemAdd'
    Shall be used to add items to be tracked.
    
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

---
+++ app/src/main/java/self/premi/sanjeev/nayati/ItemAddActivity.java
@@ -0,0 +1,43 @@
+/*
+ * [Nayati] ItemAddActivity.java
+ *
+ * Implements activity to add an item to be tracked.
+ *
+ * (c) 2015 Sanjeev Premi (spremi@ymail.com)
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ *                          (http://spdx.org/licenses/BSD-3-Clause.html)
+ *
+ */
+
+
+package self.premi.sanjeev.nayati;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+
+public class ItemAddActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_item_add);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+}
---
+++ app/src/main/java/self/premi/sanjeev/nayati/ItemAddActivityFragment.java
@@ -0,0 +1,36 @@
+/*
+ * [Nayati] ItemAddActivityFragment.java
+ *
+ * Implements fragment to add an item to be tracked.
+ *
+ * (c) 2015 Sanjeev Premi (spremi@ymail.com)
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ *                          (http://spdx.org/licenses/BSD-3-Clause.html)
+ *
+ */
+
+
+package self.premi.sanjeev.nayati;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * Fragment to add an item to be tracked.
+ */
+public class ItemAddActivityFragment extends Fragment {
+
+    public ItemAddActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_item_add, container, false);
+    }
+}
