1c67daa36d32e8db1c5eada3716ff5af1e33cbf3
Sanjeev Premi <spremi@ymail.com>
2016-01-03 18:37:06 +0530

    app: Add activity 'ItemList'
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

---
+++ app/src/main/java/self/premi/sanjeev/nayati/ItemListActivityFragment.java
@@ -0,0 +1,36 @@
+/*
+ * [Nayati] ItemListActivityFragment.java
+ *
+ * Implements fragment to list items being tracked.
+ *
+ * (c) 2015 Sanjeev Premi (spremi@ymail.com)
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ *                          (http://spdx.org/licenses/BSD-3-Clause.html)
+ *
+ */
+
+
+package self.premi.sanjeev.nayati;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * Fragment to view list of items being tracked.
+ */
+public class ItemListActivityFragment extends Fragment {
+
+    public ItemListActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_item_list, container, false);
+    }
+}
