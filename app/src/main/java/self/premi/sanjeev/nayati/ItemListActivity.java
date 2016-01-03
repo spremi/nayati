4d4624b8d927ea915df6ac1a3fee3311f439215d
Sanjeev Premi <spremi@ymail.com>
2016-01-03 19:13:29 +0530

    activity: item-list: Add transition to 'Item Add' activity
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

--- app/src/main/java/self/premi/sanjeev/nayati/ItemListActivity.java
+++ app/src/main/java/self/premi/sanjeev/nayati/ItemListActivity.java
@@ -13,9 +13,9 @@
 
 package self.premi.sanjeev.nayati;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -34,8 +34,12 @@ public class ItemListActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                /*
+                 * Create intent to start 'add' activity
+                 */
+                Intent i = new Intent(ItemListActivity.this, ItemAddActivity.class);
+
+                startActivity(i);
             }
         });
     }
