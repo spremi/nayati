544f4158d358e19d335c2668a56161a1a1388bb6
Sanjeev Premi <spremi@ymail.com>
2016-01-09 19:12:10 +0530

    activity: item-detail: Remove menu creation & handling
    Prepare to move menu creation & handling to the fragment.
    
    This commit is just a reminder for the movement.
    
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

--- app/src/main/java/self/premi/sanjeev/nayati/ItemDetailActivity.java
+++ app/src/main/java/self/premi/sanjeev/nayati/ItemDetailActivity.java
@@ -17,8 +17,6 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 
 public class ItemDetailActivity extends AppCompatActivity {
@@ -57,50 +55,7 @@ public class ItemDetailActivity extends AppCompatActivity {
     }
 
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        //
-        // Inflate the menu and add to the action bar
-        //
-        getMenuInflater().inflate(R.menu.menu_item_detail, menu);
-
-        return true;
-    }
-
-
     public String getTrackNum() {
         return trackNum;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-
-        int id = item.getItemId();
-
-        if (id == R.id.item_detail_action_delete) {
-            actionItemDelete();
-
-            return true;
-        }
-        else if (id == R.id.item_detail_action_refresh) {
-            actionItemRefresh();
-
-            return true;
-        }
-        else {
-            return super.onOptionsItemSelected(item);
-        }
-    }
-
-    /**
-     * Delete the current item and its history
-     */
-    private void actionItemDelete() {
-    }
-
-    /**
-     * Refresh details of current item
-     */
-    private void actionItemRefresh() {
     }
 }
