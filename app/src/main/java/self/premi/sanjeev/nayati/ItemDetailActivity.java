266f1f8bf9127142456fd6ff142918f4076381b7
Sanjeev Premi <spremi@ymail.com>
2016-01-09 15:39:36 +0530

    activity: item-detail: Handle click on fab
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

--- app/src/main/java/self/premi/sanjeev/nayati/ItemDetailActivity.java
+++ app/src/main/java/self/premi/sanjeev/nayati/ItemDetailActivity.java
@@ -15,7 +15,6 @@ package self.premi.sanjeev.nayati;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -47,8 +46,10 @@ public class ItemDetailActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                /*
+                 * Return to previous activity
+                 */
+                finish();
             }
         });
     }
