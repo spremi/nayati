a10186c84806c158be9bb08f8ba58372550a44b2
Sanjeev Premi <spremi@ymail.com>
2016-02-07 04:27:37 +0530

    activity: item-list: Add logo to the toolbar
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

--- app/src/main/java/self/premi/sanjeev/nayati/ItemListActivity.java
+++ app/src/main/java/self/premi/sanjeev/nayati/ItemListActivity.java
@@ -16,6 +16,7 @@ package self.premi.sanjeev.nayati;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -45,6 +46,12 @@ public class ItemListActivity extends AppCompatActivity {
         setContentView(R.layout.activity_item_list);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        ActionBar actionBar = getSupportActionBar();
+
+        if (actionBar != null) {
+            actionBar.setLogo(R.drawable.ic_nayati);
+        }
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
