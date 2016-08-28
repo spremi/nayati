02a4392033943b10b60af23bf44c889d9632decb
Sanjeev Premi <spremi@ymail.com>
2016-08-28 18:10:25 +0530

    activity: item-detail: rv-adapter: Remove unused imports
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

--- app/src/main/java/self/premi/sanjeev/nayati/ItemDetailRvAdapter.java
+++ app/src/main/java/self/premi/sanjeev/nayati/ItemDetailRvAdapter.java
@@ -13,7 +13,6 @@
 
 package self.premi.sanjeev.nayati;
 
-import android.content.Intent;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
